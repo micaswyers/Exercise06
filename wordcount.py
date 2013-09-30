d6ad40a39aaee7cc84d75aabee8a817266739e7d
Hackbright Student <user@chromebox-003.(none)>
2013-09-30 16:34:37 -0700

    Sorts all the right ways, albeit slowly

--- wordcount.py
+++ wordcount.py
@@ -1,10 +1,3 @@
-"""
-open a file from the command line
-read a line of the file
-create a dictionary that makes a key of each new, space-separated word in the text file
-for every occurence of the key in the text file, add one to the corresponding value
-print dictionary contents to the screen
-"""
 
 from sys import argv
 
@@ -18,35 +11,63 @@ for line in filename:
     words = line.split(" ")
     
     for word in words:
-        lower_word = word.lower().rstrip('\n.,?')
+        lower_word = word.lower().strip('\n.,?"--;$(\'[_*\r')
         number_of_times = wordcount.get(lower_word, 0)
         number_of_times += 1
         wordcount[lower_word] = number_of_times      
-"""
-for key, value in wordcount.iteritems():
-    print key, value
-"""
 
-keys = wordcount.keys()
 
-sorted_keys = sorted(keys)
+
+def print_alphabetically(dictionary):
+
+    keys = dictionary.keys()
+
+    sorted_keys = sorted(keys)
+
+    for key in sorted_keys:
+        print key, dictionary[key]
 
 print "Keys sorted alphabetically:"
-for key in sorted_keys:
-    print key, wordcount[key]
+print_alphabetically(wordcount)
+
+
+def print_by_frequency(dictionary):
+
+    values = dictionary.values()
+
+    sorted_values = sorted(values)
+
+    max_value = sorted_values[-1]
+
+
+    for i in range(1, max_value + 1):
+        for key, value in dictionary.iteritems():
+            if value == i:
+                print key , value
 
 print "Keys sorted by frequency:"
+print_by_frequency(wordcount)
 
 
-values = wordcount.values()
+print "Keys sorted by frequency alphabetically:"
 
-sorted_values = sorted(values)
 
-max_value = sorted_values[-1]
+def print_frequency_alphabetically(dictionary):
+    values = dictionary.values()
+    super_sorted_list = {}
 
-super_sorted_list = []
-for i in range(1, max_value + 1):
-    for key, value in wordcount.iteritems():
-        if value == i:
-            print key , value
+    for value in values:
+        super_sorted_list[value] = []
 
+
+
+    for key in dictionary:
+        super_sorted_list[dictionary[key]].append(key)
+
+    for each in super_sorted_list:
+        super_sorted_list[each] = sorted(super_sorted_list[each])
+
+    print_alphabetically(super_sorted_list)
+
+
+print_frequency_alphabetically(wordcount)
