1a47fa4a93b23c2457ef5a41dba027e35244665b
Hackbright Student <user@chromebox-003.(none)>
2013-09-30 12:58:43 -0700

    Version with print frequencies

--- wordcount.py
+++ wordcount.py
@@ -22,7 +22,31 @@ for line in filename:
         number_of_times = wordcount.get(lower_word, 0)
         number_of_times += 1
         wordcount[lower_word] = number_of_times      
-
+"""
 for key, value in wordcount.iteritems():
     print key, value
+"""
+
+keys = wordcount.keys()
+
+sorted_keys = sorted(keys)
+
+print "Keys sorted alphabetically:"
+for key in sorted_keys:
+    print key, wordcount[key]
+
+print "Keys sorted by frequency:"
+
+
+values = wordcount.values()
+
+sorted_values = sorted(values)
+
+max_value = sorted_values[-1]
+
+super_sorted_list = []
+for i in range(1, max_value + 1):
+    for key, value in wordcount.iteritems():
+        if value == i:
+            print key , value
 
